2b9cd1209c8a95e22c12fd8f14ba5407b4aa218a
gitpasswordis147258 <848862551@qq.com>
2023-04-28 21:01:00 +0800

    更新文档+修复css无效问题

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.js'),
+      // entry: path.resolve(__dirname, 'src/plugins/colorspick.js'),
+      name: 'ColorsPick',
+      fileName: (format) => `colorspick.${format}.js`
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
